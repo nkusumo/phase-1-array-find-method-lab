4e000a40e56425c9acac40f959aec40e0c8ef7be
nkusumo <nis.kusumo@gmail.com>
2021-05-19 08:44:35 -0700

    commit tree ed60414e8e1ec9614b6da0cdeea7f5df2b9edbaf

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+const wins = function(array) {
+    return array.result === "W";
+}
+
+function superbowlWin(array) {
+    if (array.find(wins)) {
+        return array.find(wins).year;
+    } else {
+        return undefined;
+    }
+}
